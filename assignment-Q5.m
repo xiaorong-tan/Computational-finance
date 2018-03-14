d8265a171589203a18ee393959d4305bf3ba1407
xiaorong-tan <tanxiaorong104@hotmail.com>
2018-03-14 19:33:09 +0000

    Update assignment-Q5.m

--- assignment-Q5.m
+++ assignment-Q5.m
@@ -2,33 +2,45 @@ stock1 = load("CCH.csv");
 stock2 = load("GFS.csv");
 stock3 = load("SKY.csv");
 [rows,cols] = size(stock1);
-return1 = zeros(rows,1);
-return2 = zeros(rows,1);
-return3 = zeros(rows,1);
-for i = 2:rows
+for i = 1:cols
+    stock1(:,i) = stock1(:,i) - mean(stock1(:,i));
+    stock1(:,i) = stock1(:,i) / std(stock1(:,i));
+    stock2(:,i) = stock2(:,i) - mean(stock2(:,i));
+    stock2(:,i) = stock2(:,i) / std(stock2(:,i));
+    stock3(:,i) = stock3(:,i) - mean(stock3(:,i));
+    stock3(:,i) = stock3(:,i) / std(stock3(:,i));
+end
+return1 = zeros(rows/2,1);
+return2 = zeros(rows/2,1);
+return3 = zeros(rows/2,1);
+returnT1 = zeros(rows/2,1);
+returnT2 = zeros(rows/2,1);
+returnT3 = zeros(rows/2,1);
+for i = 1:rows/2
     return1(i,1) = stock1(i,4) - stock1(1,4);
     return2(i,1) = stock2(i,4) - stock2(1,4);
     return3(i,1) = stock3(i,4) - stock3(1,4);
 end
-% non-mispricing naiveMV model
-nonM_trainRe1 = mean(return1(1:rows/2));
-nonM_trainRe2 = mean(return2(1:rows/2));
-nonM_trainRe3 = mean(return3(1:rows/2));
-testRe1 = mean(return1(rows/2+1:rows));
-testRe2 = mean(return2(rows/2+1:rows));
-testRe3 = mean(return3(rows/2+1:rows));
-re = [return1 return2 return3];
-nonM_trainRet = re(1:rows/2,:);
-testRet = re(rows/2+1:rows,:);
-for i = 1:3
-    nonM_trainRet(:,i) = nonM_trainRet(:,i) - mean(nonM_trainRet(:,i));
-    nonM_trainRet(:,i) = nonM_trainRet(:,i) / std(nonM_trainRet(:,i));
-    testRet(:,i) = testRet(:,i) - mean(testRet(:,i));
-    testRet(:,i) = testRet(:,i) / std(testRet(:,i));
+for i = 1:rows/2
+    returnT1(i,1) = stock1(rows/2+i,4) - stock1(rows/2+1,4);
+    returnT2(i,1) = stock2(rows/2+i,4) - stock2(rows/2+1,4);
+    returnT3(i,1) = stock3(rows/2+i,4) - stock3(rows/2+1,4);
 end
-nonM_ECov = cov(nonM_trainRet);
-NPts = 100;
-nonM_ERet = [nonM_trainRe1 nonM_trainRe2 nonM_trainRe3]';
+% non-mispricing naiveMV model
+trainRe1 = mean(return1(1:rows/2));
+trainRe2 = mean(return2(1:rows/2));
+trainRe3 = mean(return3(1:rows/2));
+testRe1 = mean(returnT1(1:rows/2));
+testRe2 = mean(returnT2(1:rows/2));
+testRe3 = mean(returnT3(1:rows/2));
+re = [return1 return2 return3];
+reT = [returnT1 returnT2 returnT3];
+trainRet = re(1:rows/2,:);
+testRet = reT(1:rows/2,:);
+
+nonM_ECov = cov(trainRet);
+NPts = 10;
+nonM_ERet = [trainRe1 trainRe2 trainRe3]';
 % naiveMV model wiht non-mispricing values
 [nonM_PRisk, nonM_PRoR, nonM_PWts] = naiveMV(nonM_ERet, nonM_ECov, NPts);
 nonM_efficientRet = zeros(rows/2,NPts);
@@ -83,14 +95,18 @@ for i = 1:rows/2
     efficientRetAverage(i,1) = mean(efficientRet(i,:));
     efficientRetAverage1(i,1) = mean(efficientRet1(i,:));
 end
+testRetAvg = zeros(rows/2,1);
+for i = 1:rows/2
+    testRetAvg(i) = mean(testRet(i,:));
+end
 figure;
 box on;
 hold on;
 grid on;
 plot(efficientRetAverage,'g','LineWidth',2);
 plot(efficientRetAverage1,'r','LineWidth',2);
-plot(nonM_efficientRetAverage,'k','LineWidth',2)
-legend("MV model","MacKinlay & Pastor model","MV model with non-mispricing values",'Location','northwest');
+plot(testRetAvg,'k','LineWidth',2);
+legend("MV model","MacKinlay & Pastor model","True values",'Location','northwest');
 title("MacKinlay & Pastor model",'FontSize',16);
 xlabel("Days",'FontSize',14);
 ylabel("Returns",'FontSize',14);
@@ -98,13 +114,9 @@ ylabel("Returns",'FontSize',14);
 % error
 MVerror = zeros(380,1);
 Macerror = zeros(380,1);
-trueValue = zeros(380,1);
 for i = 1:380
-    trueValue(i) = mean(testRet(i,:));
-end
-for i = 1:380
-    MVerror(i) = efficientRetAverage(i) - trueValue(i);
-    Macerror(i) = efficientRetAverage1(i) - trueValue(i);
+    MVerror(i) = efficientRetAverage(i) - testRetAvg(i);
+    Macerror(i) = efficientRetAverage1(i) - testRetAvg(i);
 end
 figure;
 boxplot([MVerror,Macerror],'Notch','on','Labels',{'Mean-variance model','MacKinlay & Pastor model'});
